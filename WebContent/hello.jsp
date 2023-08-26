4b1628dfe108046d8f79516a5ca190f2c7de53f5
Praveen <praveensingam4@gmail.com>
2023-08-26 11:13:44 +0530

    spring mvc project

---
+++ WebContent/hello.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Home JSP</title>
+</head>
+<body>
+  <H3>Hello from jsp file..</H3>
+</body>
+</html>
---
+++ src/main/java/com/sp/mvc/SpringMvcController.java
@@ -0,0 +1,12 @@
+package com.sp.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SpringMvcController {
+	@RequestMapping(value="/getData")
+  public String getData() {
+	  return "hello";
+  }
+}
